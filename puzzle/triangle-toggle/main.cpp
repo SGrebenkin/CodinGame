4864d8f60c3a201e376da97dd7ae1de13bd701a9
Sergey Grebenkin <sergey.grebenkin@gmail.com>
2024-11-28 22:36:30 +0100

    CodinGame: puzzle/triangle-toggle

--- puzzle/triangle-toggle/main.cpp
+++ puzzle/triangle-toggle/main.cpp
@@ -14,20 +14,6 @@ bool pnpoly(const vector<pair<int, int>>& triag, int x, int y) {
         int jy = triag[(i + 1) % triag.size()].first;
         int jx = triag[(i + 1) % triag.size()].second;
 
-        if ((x == ix && y == iy) || (x == jx && y == jy))
-            return true;
-
-        int miny = min(iy, jy);
-        int maxy = max(iy, jy);
-
-        // point is a part of vertical line
-        if (ix == x && jx == x)
-            return true;
-
-        // point lies on horizontal line
-        if (iy == y && jy == iy && x >= min(ix, jx) && x <= max(ix, jx))
-            return true;
-
         // calculate vector product
         int res = (x - ix) * (jy - iy) - (y - iy) * (jx - ix);
         if (res == 0)
@@ -36,10 +22,7 @@ bool pnpoly(const vector<pair<int, int>>& triag, int x, int y) {
         // save sign of vector product
         // if the point is inside the triangle, all
         // vector products should have the same sign
-        if (res > 0)
-            ctr++;
-        else if (res < 0)
-            ctr--;
+        ctr += (res >= 0) ? 1 : ((res <= 0) ? -1 : 0);
     }
 
     // check vector product after traversal
@@ -64,28 +47,15 @@ int main()
         board.push_back(vector<char>(wi, '*'));
 
     for (int i = 0; i < how_many_triangles; i++) {
-        int x_1;
-        int y_1;
-        int x_2;
-        int y_2;
-        int x_3;
-        int y_3;
+        int x_1, y_1, x_2, y_2, x_3, y_3;
         cin >> x_1 >> y_1 >> x_2 >> y_2 >> x_3 >> y_3; cin.ignore();
 
-        vector<pair<int, int>> triag;
-        triag.push_back({ y_1, x_1 });
-        triag.push_back({ y_2, x_2 });
-        triag.push_back({ y_3, x_3 });
+        vector<pair<int, int>> triag{ {y_1, x_1}, {y_2, x_2}, {y_3, x_3} };
 
-        int x_min = std::min({ x_1, x_2, x_3 });
-        int y_min = std::min({ y_1, y_2, y_3 });
-        int x_max = std::max({ x_1, x_2, x_3 });
-        int y_max = std::max({ y_1, y_2, y_3 });
-
-        x_max = min(x_max, wi - 1);
-        x_min = max(x_min, 0);
-        y_max = min(y_max, hi - 1);
-        y_min = max(y_min, 0);
+        int x_min = max(min({ x_1, x_2, x_3 }), 0);
+        int y_min = max(min({ y_1, y_2, y_3 }), 0);
+        int x_max = min(max({ x_1, x_2, x_3 }), wi - 1);
+        int y_max = min(max({ y_1, y_2, y_3 }), hi - 1);
 
         for (int i = x_min; i <= x_max; ++i)
             for (int j = y_min; j <= y_max; ++j) {
@@ -107,4 +77,6 @@ int main()
 
         cout << endl;
     }
+
+    return 0;
 }
